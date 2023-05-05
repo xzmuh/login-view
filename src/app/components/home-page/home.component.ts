b1c779a5cbc69fce60341c681d8205d73045cbff
murilotrigo_ <murilo.trigo@auditornfe.com.br>
2023-05-05 09:08:24 -0300

    funcao abrir menu e variavel de cor

--- src/app/components/home-page/home.component.ts
+++ src/app/components/home-page/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { window } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  constructor(){
+    
+  }
+
+  ngOnInit(): void {
+    this.getWidthWindow();
+  }
+
+  openMenu(){
+    const navegacao = (document.querySelector(".menu-lateral") as HTMLLIElement )
+    navegacao.classList.toggle("openMenu")
+  }
+
+  getWidthWindow(){
+    const widthWindow = screen.width;
+    const navegacao = (document.querySelector(".menu-lateral") as HTMLLIElement )
+    if (widthWindow > 500){
+      navegacao.classList.add("openMenu")
+    }
+  }
 
 }
